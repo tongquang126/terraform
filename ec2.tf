2b06837692483e5fb94e90ef9fc9abf25b1065f5
tongquang126 <61310408+tongquang126@users.noreply.github.com>
2022-02-11 11:52:07 +0800

    Update ec2.tf
    Rename the aws_instance resource

--- ec2.tf
+++ ec2.tf
@@ -12,7 +12,7 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-resource "aws_instance" "new_ec2" {
+resource "aws_instance" "my_ec2" {
   ami           = "ami-0a8b4cd432b1c3063" 
   instance_type = "t2.micro"
 }
