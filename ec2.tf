450d76c617df792a3b4738fe66b5340534b727e9
tongquang126 <61310408+tongquang126@users.noreply.github.com>
2022-02-11 11:58:37 +0800

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "3.73.0"
-    }
-  }
-}
-
 provider "aws" {
-  profile = "default"
   region  = "us-east-1"
 }
 
 resource "aws_instance" "my_ec2" {
   ami           = "ami-0a8b4cd432b1c3063" 
   instance_type = "t2.micro"
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.my_ec2.private_ip
 }
